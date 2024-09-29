4a1f295c2163a140b799c8dbf5902f763da0eb75
KWAK-JINHO <kjhkjhkjh1012@gmail.com>
2024-09-29 23:11:00 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 120 ms, Memory: 110896 KB -BaekjoonHub

--- 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
@@ -1,16 +1,7 @@
-def coin_change(cents):
-    coins = [25, 10, 5, 1]  # 센트 단위로 표현된 동전 가치
-    change = []
-    
-    for coin in coins:
-        count = cents // coin
-        change.append(str(count))
-        cents %= coin
-    
-    return ' '.join(change)
+n = int(input())
 
-T = int(input())  # 테스트 케이스의 수
-
-for _ in range(T):
-    cents = int(input())  # 센트 단위로 직접 입력받음
-    print(coin_change(cents))
+for _ in range(n):
+	money = int(input())
+	for i in [25, 10, 5, 1]:
+		print(money//i, end=' ')
+		money = money%i
