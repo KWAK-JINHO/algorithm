d9566242c1a28dea63c15e3d6f7d86d2925b0cc6
KWAK-JINHO <kjhkjhkjh1012@gmail.com>
2026-01-11 23:27:03 +0900

    인프런 3 번 재풀이

--- inflearn/q03/Problem_03.java
+++ inflearn/q03/Problem_03.java
@@ -80,5 +80,22 @@ public class Problem_03 {
 //        String str = sc.nextLine();
 //        System.out.println(solution(str));
 //    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        System.out.println(solution(input));
+    }
 
+    public static String solution(String input) {
+        String[] arr = input.split(" ");
+        int strLength = 0;
+        int idx = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].length() > strLength) {
+                strLength = arr[i].length();
+                idx = i;
+            }
+        }
+        return arr[idx];
+    }
 }
